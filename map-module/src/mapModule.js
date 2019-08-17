4a0e6f736bbae98bd371510ff5038c29f7d66ac3
villiam@kth.se <villiam@kth.se>
2019-08-17 23:18:32 +0200

    Added OpenLayers map to the module

--- map-module/src/mapModule.js
+++ map-module/src/mapModule.js
@@ -1,8 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import TileWMS from 'ol/source/TileWMS';
 
-function MapModule(props)
-{
- return <h1>MapModule 4: {props.name}</h1>;
+
+class MapModule extends Component {
+  constructor(props) {
+    super(props);
+
+    this.map = new Map({
+      target: null,
+      layers: [
+        new TileLayer({
+          extent: [-13884991, 2870341, -7455066, 6338219],
+          source: new TileWMS({
+            url: 'http://localhost/MapService/wms.axd/vmap0',
+            params: {'LAYERS': 'Land', 'TILED': true},
+            // Countries have transparency, so do not fade tiles:
+            transition: 0
+          })
+        })
+      ],
+      view: new View({
+        center: [0, 0],
+        zoom: 2
+      })
+    });
+  }
+
+  componentDidMount() {
+    this.map.setTarget("mapContainer");
+  }
+
+  render()  {
+    return <div id="mapContainer" style={{ width: "100%", height: "360px" }}></div>
+  }
 }
 
 export default MapModule;
+
+
+
+ 
